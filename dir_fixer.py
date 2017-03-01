f83494a5be62699dbdca3f9f83341e5916289e3b
Simulacrum <simulacrumsurface@protonmail.com>
2017-03-01 21:18:05 +0100

    Added an exception

--- dir_fixer.py
+++ dir_fixer.py
@@ -30,7 +30,7 @@ def main():
     else:
         path = "".join((path[0], "/"))
         #TODO Check if path is existing
-        if os.path.isdir(path) == False:
+        if os.path.isdir(path) == False:ne :
             print("[-] The given path does not lead to a directory.\nExiting...")
             exit(0)
         else:
@@ -46,16 +46,19 @@ def main():
                     new_path_name = "".join((path, folder, "lol_i_hope_this_fileextension_is_random_enough/"))
                     if options.verbose == True:
                         print("[+] Renaming subDIR.")
+
                     os.rename(old_path_name, new_path_name)
                     in_new_path, *no_usage = os.walk(new_path_name)
-
-                    for file in in_new_path[2]:
-                        file_path = "".join((new_path_name, file))
-                        shutil.move(file_path, path)
-                        if options.verbose == True:
-                            print("moving {} -> {}".format(file, path))
-
-                    shutil.rmtree(new_path_name)
+                    try:
+                        for file in in_new_path[2]:
+                            file_path = "".join((new_path_name, file))
+                            shutil.move(file_path, path)
+                            if options.verbose == True:
+                                print("moving {} -> {}".format(file, path))
+                        shutil.rmtree(new_path_name)
+                    except:
+                        print("[-] An Error occured in directory {}\nSkipping this...".format(old_path_name))
+                        os.rename(new_path_name, old_path_name)
     return
 if __name__ == '__main__':
     main()
